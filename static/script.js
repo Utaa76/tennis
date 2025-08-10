6c5fb102539a6685373db8252091e32fefb9e67a
Yanis Verdier <yanis.verdier@ecole.ensicaen.fr>
2025-08-10 18:44:51 +0200

    lien maj

--- static/script.js
+++ static/script.js
@@ -19,7 +19,7 @@ form.addEventListener('submit', async (e) => {
 	resultDiv.textcontent = 'Chargement...';
 
 	try {
-		const response = await fetch('http://127.0.0.1:8000/predict', {
+		const response = await fetch('https://tennis-8gw3.onrender.com/predict', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify(data)
